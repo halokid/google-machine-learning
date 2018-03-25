1438f4d35f4969381c332027de0dd18f9ce06cbd
82049406@qq.com <82049406@qq.com>
2018-03-25 23:34:15 +0800

    tactx

--- Lesson7.py
+++ Lesson7.py
@@ -83,6 +83,43 @@ validation_targets.describe()
 
 
 
+# ------------ 描绘经纬度  和  median_house_value 的曲线图 -----------------------------
+plt.figure(figsize=(13, 8))
+
+ax = plt.subplot(1, 2, 1)
+ax.set_title("Validation Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(validation_examples["longitude"],
+            validation_examples["latitude"],
+            cmap="coolwarm",
+            c=validation_targets["median_house_value"] / validation_targets["median_house_value"].max())
+
+ax = plt.subplot(1,2,2)
+ax.set_title("Training Data")
+
+ax.set_autoscaley_on(False)
+ax.set_ylim([32, 43])
+ax.set_autoscalex_on(False)
+ax.set_xlim([-126, -112])
+plt.scatter(training_examples["longitude"],
+            training_examples["latitude"],
+            cmap="coolwarm",
+            c=training_targets["median_house_value"] / training_targets["median_house_value"].max())
+
+
+_ = plt.plot()
+
+
+
+
+
+
+
+
 
 
 
